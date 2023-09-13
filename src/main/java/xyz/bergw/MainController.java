ccd96f8b467317d00091ffddee25117acd2a6d2b
Will Berg <william.berg08@gmail.com>
2023-09-13 11:29:36 +0100

    Add root redirect

--- src/main/java/xyz/bergw/MainController.java
+++ src/main/java/xyz/bergw/MainController.java
@@ -1,11 +1,19 @@
 package xyz.bergw;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
-public class HelloController {
+public class MainController {
+
+    @RequestMapping("/")
+    void handleFoo(HttpServletResponse response) throws IOException {
+        response.sendRedirect("http://static.bergw.xyz");
+    }
 
     @RequestMapping("/{thing}")
     String hello(@PathVariable String thing) {
